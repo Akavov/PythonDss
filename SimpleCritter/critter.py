34faa1d9f40580e832702ab45592120616135b14
ARS <arsakavov@gmail.com>
2018-03-11 17:54:29 +0300

    classes

---
+++ SimpleCritter/critter.py
@@ -0,0 +1,10 @@
+#demonstrate classes
+class Critter(object):
+    """Virtual critter"""
+    def talk(self):
+        print("Hi, This is pet - object of Critter")
+
+#main part
+crit=Critter()
+crit.talk()
+input("\n\nPress Enter to exit")
